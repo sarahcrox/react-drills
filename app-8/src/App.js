46674b59b4632e7976e3dccfce2bce9ff26407dd
sarahcrox <croxford.sarahm@gmail.com>
2020-02-26 16:46:21 -0700

    App-8 Completed

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component }from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
+class App extends Component{
+  constructor(){
+    super();
+      this.state={
+        snorlax: ""
+      };
+  }
+  componentDidMount(){
+    axios.get('https://pokeapi.co/api/v2/').then(response => {this.setState({snorlax: response.data});
+  });
+
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4>Name: {this.state.snorlax.name}</h4>
+      <h4>Forms: {this.state.snorlax.forms}</h4>
+      <h4>Moves: {this.state.snorlax.moves}</h4>
+      <h4>Species: {this.state.snorlax.species}</h4>
     </div>
   );
+}
 }
 
 export default App;
