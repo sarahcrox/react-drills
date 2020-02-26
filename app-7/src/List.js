28ea8915c6c40f3f965d711122250d9fe235d110
sarahcrox <croxford.sarahm@gmail.com>
2020-02-26 16:30:50 -0700

    App-7 Completed

---
+++ app-7/src/List.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Todo from "./Todo";
+
+export default class list extends Component{
+    render(){
+        let list= this.props.tasks.map((element, index) => {
+            return <Todo key={index} task={element} />
+        });
+    return <div>{list}</div>
+    }
+}
---
+++ app-7/src/Todo.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+export default class todo extends Component{
+    render(){
+    return <p>{this.props.task}</p>
+    }
+}
